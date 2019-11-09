01fd18ffeab55aba0889d12ac8edc92d4db01d6e
lottenoorlander <lottenoorlander@gmail.com>
2019-11-09 15:38:45 +0100

    display stats

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -2,7 +2,7 @@
 
 /*hero Object*/
 const hero = {
-    name: "Lotte", 
+    name: "", 
     heroic: true,
     inventory: [],
     health: 10,
@@ -61,15 +61,58 @@ document.getElementById("bag").addEventListener("click", callequipWeapon);
 /*hero name input*/
 function namePrompt(){
     let heroName = prompt("Good afternoon mysterious stranger, what is your name?");
-
+    
     /*if empty reprompt */
     while(!heroName) {
         heroName = prompt("We don't like strangers here, tell us your name")
     }
-                
-        /*create placeholder to put name*/
-        const whatToDo = document.createElement("p")
-        whatToDo.innerHTML = `What would you like to do, ${heroName}`
+    
+    /*create placeholder to put name*/
+    const whatToDo = document.createElement("p")
+    whatToDo.innerHTML = `What would you like to do, ${heroName}`
         const nameSection = document.getElementById("name")
         nameSection.appendChild(whatToDo)
-    }
+    /*also add it to the object*/
+    hero.name = heroName;
+    displayStats(hero)
+}
+
+/* Write displayStats function that writes your hero's name, health, weapontype, weapon damage to the page. Call it at the end of your script*/
+function displayStats(person){
+    
+    /*stats you need*/
+     const nameValue = person.name
+     const healthValue = person.health
+     const weaponValue = person.weapon.type
+     const damageValue = person.weapon.damage
+     
+
+    /*place for stats to go*/
+    nameField = document.createElement("p")
+    healthField = document.createElement("p")
+    weaponField = document.createElement("p")
+    damageField = document.createElement("p")
+
+    //*IF NO WEAPON THEN DISPLAY damage empty*//
+    const damageWeaponOrNo = ()=> {
+        if(!weaponValue) {
+        return `It does no damage`          
+      }else{
+          return `It does: ${damageValue} damage`
+        }}
+
+    /*add stats to their fields*/
+    nameField.innerHTML = `Name: ${nameValue}`
+    healthField.innerHTML = `Health: ${healthValue}`
+    weaponField.innerHTML = `Weapon: ${weaponValue}`
+    damageField.innerHTML = damageWeaponOrNo()
+    
+
+    /*add all fields together*/
+    statsField = document.getElementById("stats")
+    statsField.appendChild(nameField)
+    statsField.appendChild(healthField)
+    statsField.appendChild(weaponField)
+    statsField.appendChild(damageField)
+
+}
