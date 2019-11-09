cda18c1185dd089a30ed604141cddb034ac4b685
lottenoorlander <lottenoorlander@gmail.com>
2019-11-09 11:18:37 +0100

    hero object and start on functions

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,28 @@
 // Write your JS here
+
+/*hero Object*/
+const hero = {
+    name: "Lotte", 
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "sword",
+        damage: 2
+    }
+}
+
+/*rest function*/
+function rest(){
+
+}
+
+/*pick up Item function */
+function pickUpItem(){
+
+}
+
+/*equip a weapon function*/
+function equipWeapon(){
+
+}
