28aece40e5c75827f8531970b5cda120a75a2cff
lottenoorlander <lottenoorlander@gmail.com>
2019-11-09 12:48:09 +0100

    added resting and picking up weapon functionality

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -7,20 +7,39 @@ const hero = {
     inventory: [],
     health: 10,
     weapon: {
-        type: "sword",
+        type: "dagger",
         damage: 2
     }
 }
 
 /*rest function*/
-function rest(){
-
+function rest(person){
+    if(person.health === 10){
+        alert("You're already at full health")
+    }
+        person.health = 10;
+        return person;
 }
+
+/*call rest function for hero when you click the inn*/
+function callRestHero(){
+    rest(hero);
+}
+document.getElementById("inn").addEventListener("click", callRestHero);
+
 
 /*pick up Item function */
-function pickUpItem(){
-
+function pickUpItem(person, weapon){
+    person.inventory.push(weapon);
 }
+
+/*call pickupitem function for hero when you click the inn*/
+const dagger = hero.weapon;
+function callpickUpDagger(){
+    pickUpItem(hero, dagger);
+}
+document.getElementById("dagger").addEventListener("click", callpickUpDagger);
+
 
 /*equip a weapon function*/
 function equipWeapon(){
